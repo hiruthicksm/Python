1c7c1eca90c8782e66e123905737a023b6489234
hiruthicksm <sm.hiruthick2021@vitstudent.ac.in>
2024-05-25 23:31:06 +0530

    comments added

--- bandnamegenerator.py
+++ bandnamegenerator.py
@@ -1,4 +1,4 @@
 print("Welcome to the band name generator")
 city = input("What's the name of the city you grew up in? \n")
 pet_name = input("What's your pet's name \n")
-print("Your band name could be " + city + " " + pet_name)
+print("Your band name could be " + city + " " + pet_name)#concatenates the city with pet name
