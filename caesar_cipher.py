f6b497e5563e10483e4cbc1e8fe604ef42041362
hiruthicksm <sm.hiruthick2021@vitstudent.ac.in>
2024-06-05 20:55:15 +0530

    Create caesar_cipher.py
    Python program that computes ceaser cipher encryption and decryption

---
+++ caesar_cipher.py
@@ -0,0 +1,63 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+                                                         
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+def caesar(st_text,shift_no,direc):
+  cipher=""
+
+  for i in st_text:
+        if i in alphabet:
+          ind=int(alphabet.index(i))
+          if direc=="encode":
+            shift_no=shift_no%26
+            new_ind=ind+shift_no   
+            alp=alphabet[new_ind]
+            cipher=cipher+alp
+
+
+          if direc=="decode":
+            shift_no=shift_no%26
+            newind=ind-shift_no
+            dcy=alphabet[newind]
+            cipher=cipher+dcy
+        else:
+          cipher=cipher+i
+  print(f"The {direction}d text is {cipher}")
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+flag=True
+while flag:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n")) 
+  caesar(text,shift,direction)
+  ip=input("To continue press 'yes',to exit press'no': ")
+  
+  if ip=="no":
+    flag=False
+    print("Good bye!")
+
+
+
+
+ 
+
+
+
+
+
+
